18b8307f2b0e2bd1708fdca9db0634006ab67240
Kristofer <kris@youngers.org>
2021-06-01 12:58:20 -0400

    adding python repl

---
+++ code/python/coderepl.js
@@ -0,0 +1,77 @@
+var editor = ace.edit("editor", {
+    theme: "ace/theme/tomorrow_night_blue",
+    mode: "ace/mode/python",
+    autoScrollEditorIntoView: true,
+    fontSize: 16,
+    maxLines: 15,
+    minLines: 15
+});
+
+var consolewin = ace.edit("consolewin");
+consolewin.setOptions({
+    theme: "ace/theme/terminal",
+    autoScrollEditorIntoView: true,
+    fontSize: 16,
+    maxLines: 8,
+    minLines: 8
+});
+consolewin.renderer.setScrollMargin(10, 10, 10, 10);
+
+var
+    preamble = `
+# pre
+`;
+
+var postamble = `
+# post
+
+`;
+
+var selectionText = "";
+
+window.addEventListener('message',
+
+    function(e) {
+        // Sandboxed iframes which lack the 'allow-same-origin'
+        // header have "null" rather than a valid origin. This means you still
+        // have to be careful about accepting data via the messaging API you
+        // create. Check that source, and validate those inputs!
+        var frame = document.getElementById('sandboxed');
+        if (e.origin === "null" && e.source === frame.contentWindow) {
+            consolewin.setValue(e.data);
+            consolewin.clearSelection(); // This will remove the highlight over the text
+        }
+    });
+
+function evaluate() {
+    var frame = document.getElementById('sandboxed');
+    // var code = document.getElementById('code').value;
+    var code = preamble + editor.getValue() + postamble;
+    clearConsoleWin();
+    //console.log(code);
+    // Note that we're sending the message to "*", rather than some specific
+    // origin. Sandboxed iframes which lack the 'allow-same-origin' header
+    // don't have an origin which you can target: you'll have to send to any
+    // origin, which might alow some esoteric attacks. Validate your output!
+    frame.contentWindow.postMessage(code, '*');
+}
+
+function clearConsoleWin() {
+    consolewin.setValue("");
+    consolewin.clearSelection(); // This will remove the highlight over the text
+}
+
+function selectAllEditor() {
+    editor.selectAll();
+}
+
+function deleteSelectedCode() {
+    selectionText = editor.getSelection();
+    editor.insert("");
+}
+
+document.getElementById('runjs').addEventListener('click', runit);
+document.getElementById('clear').addEventListener('click', clearConsoleWin);
+document.getElementById('selall').addEventListener('click', selectAllEditor);
+document.getElementById('clearcode').addEventListener('click', deleteSelectedCode);
+// document.getElementById("sandboxed").style.display = "none";
